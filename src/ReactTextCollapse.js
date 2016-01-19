f22903fb9fc9d0ee234e3c57a81face8bb8e8488
Robert Chang <cht8687@gmail.com>
2016-01-19 23:29:31 +1100

    Add in toggle

--- src/ReactTextCollapse.js
+++ src/ReactTextCollapse.js
@@ -1,22 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Motion, spring } from "react-motion";
 
 export default class ReactTextCollapse extends Component {
   static propTypes = {
-    
+    children: PropTypes.node.isRequired,
+    options: PropTypes.object.isRequired
   };
 
   constructor(props) {
     super(props);
 
+    const { options: { Collapse } } = this.props;
     this.state = {
+      Collapse: Collapse,
+      height: 100
     }
   }
 
+  componentDidMount() {
+    //const height = ReactDOM.findDOMNode(this).offsetHeight;
+    //this.setState({height: height});
+  }
+
+  renderHelperText() {
+    const { options: { collapseText, expandText } } = this.props; 
+    const { Collapse } = this.state;
+    if (Collapse) {
+      return <div style={{float:'left'}}>{expandText}</div>
+    } else {
+      return <div style={{float:'left'}}>{collapseText}</div>
+    }
+  }
+
+  toggleAction() {
+    let { Collapse } = this.state;
+    Collapse = !Collapse;
+    this.setState({ Collapse});
+  }
+
   render() {
+    const { children } = this.props;
+    const { Collapse, height } = this.state;
     return (
-      <div>
-        hello React!
-      </div>
+      <Motion
+        defaultStyle={{h: 0}}
+        style={{h: spring(Collapse ? 0 : height)}}>
+        {
+          ({h}) => (
+            <div>
+              <div 
+                  style={{
+                    display: `block`,
+                    overflow: `hidden`,
+                    height:`${h}`
+                  }}>
+                {children}
+              </div>
+              <div onClick={this.toggleAction.bind(this)}>{this.renderHelperText()}</div>
+            </div>
+          )
+        }
+      </Motion>
     );
   }
 }
--- src/example/example.js
+++ src/example/example.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import ReactTextCollapse from '..';
 
+
+const OPTIONS = {
+	Collapse: false,
+  collapseText: '... show more',
+  expandText: 'show less'
+}
+
 class App extends Component {
 
   render() {
 
     return (
       <div>
-        <ReactTextCollapse />
+        <ReactTextCollapse
+        	options={OPTIONS}
+        > 
+        <p>
+           React text collapse is awesome. React text collapse is awesome.<br />
+           React text collapse is awesome. React text collapse is awesome.<br />
+           React text collapse is awesome. React text collapse is awesome.<br />
+           React text collapse is awesome. React text collapse is awesome.<br />
+           React text collapse is awesome. React text collapse is awesome.<br />
+           React text collapse is awesome. React text collapse is awesome.<br />
+           React text collapse is awesome. React text collapse is awesome.<br />
+           React text collapse is awesome. React text collapse is awesome.<br />
+        </p>
+        </ReactTextCollapse> 
       </div>
     );
   }
@@ -19,4 +39,3 @@ appRoot.id = 'app';
 document.body.appendChild(appRoot);
 
 render(<App />, appRoot);
-  
